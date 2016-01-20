32fcaa18c374b9e782b5a82b5f63c3a83f54cd7c
Benjamin M. Brown <benjaminbb@gmail.com>
2016-01-19 22:12:52 -0600

    Fixed typo

--- simple-server.js
+++ simple-server.js
@@ -3,5 +3,5 @@ console.log("Creating server");
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
-}).lisen(1337, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
 console.log("Server running at http://127.0.0.1");
